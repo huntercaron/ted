f4055ace0946efbcfe6fcee3136f4404a80e8d65
Hunter Caron <huntercaron@mac.com>
2018-04-12 19:31:53 -0400

    test

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
     body: "Hello, Worldddd"
+  });
+
+  fs.readdir(path, function (err, items) {
+    console.log(items);
+
+    for (var i = 0; i < items.length; i++) {
+      console.log(items[i]);
+    }
   });
 }

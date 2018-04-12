e18d751ab792d2d9298bfdae95a11f0c0f535938
Hunter Caron <huntercaron@mac.com>
2018-04-12 19:47:58 -0400

    testing functinos

---
+++ functions-source/test.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+exports.handler = function (event, context, callback) {
+  
+
+  fs.readdir('.', function (err, items) {
+    console.log(items);
+
+    let logs = [];
+
+    for (var i = 0; i < items.length; i++) {
+      console.log(items[i]);
+      logs.push(items[i])
+    }
+
+    callback(null, {
+      statusCode: 200,
+      body: logs.toString()
+    });
+  });
+}

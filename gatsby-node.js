154946b889ca5a29a8872df9a0c5224c6b3054a8
Hunter Caron <huntercaron@mac.com>
2018-04-14 12:15:45 -0400

    switching to smaller data files

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,19 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const fs = require('fs');
+// const fs = require('fs');
+const fs = require(`fs-extra`)
 
-const fromFilePath = './ted-data.json'
+const fromFilePath = './ted-data-big.json'
 const toFilePath = './public/ted-data.json'
 
 
 exports.onPreBuild = () => {
-  fs.readFile(fromFilePath, (err, data) => {
-    if (err) throw err;
+  // fs.readFile(fromFilePath, (err, data) => {
+  //   if (err) throw err;
 
-    fs.writeFile(toFilePath, data, (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
-    });
-  });
+  //   fs.writeFile(toFilePath, data, (err) => {
+  //     if (err) throw err;
+  //     console.log('The file has been saved!');
+  //   });
+  // });
+  // fs.copy(fromFilePath, toFilePath);
+
+  // fs.copyFile(fromFilePath, toFilePath, (err) => {});
 }

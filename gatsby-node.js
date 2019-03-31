a2869843fbb7aa8130109da601929ec3e3ca801f
Hunter Caron <huntercaron@me.com>
2019-03-31 19:42:01 +0200

    pre-updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,20 +7,20 @@
 // const fs = require('fs');
 // const fs = require(`fs-extra`)
 
-const fromFilePath = './ted-data-big.json'
-const toFilePath = './public/ted-data.json'
+// const fromFilePath = './ted-data-big.json'
+// const toFilePath = './public/ted-data.json'
 
 
-exports.onPreBuild = () => {
-  // fs.readFile(fromFilePath, (err, data) => {
-  //   if (err) throw err;
+// exports.onPreBuild = () => {
+//   // fs.readFile(fromFilePath, (err, data) => {
+//   //   if (err) throw err;
 
-  //   fs.writeFile(toFilePath, data, (err) => {
-  //     if (err) throw err;
-  //     console.log('The file has been saved!');
-  //   });
-  // });
-  // fs.copy(fromFilePath, toFilePath);
+//   //   fs.writeFile(toFilePath, data, (err) => {
+//   //     if (err) throw err;
+//   //     console.log('The file has been saved!');
+//   //   });
+//   // });
+//   // fs.copy(fromFilePath, toFilePath);
 
-  // fs.copyFile(fromFilePath, toFilePath, (err) => {});
-}
+//   // fs.copyFile(fromFilePath, toFilePath, (err) => {});
+// }

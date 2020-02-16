ea06aa68f4d93559e005a4b93d3eba57d0916199
Hunter Caron <huntercaron@me.com>
2020-02-16 12:21:19 +0100

    fixing styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-layout`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-layout`,
+    `gatsby-plugin-styled-components`,
+  ],
 }

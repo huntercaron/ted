8338815ba1102a7f3db5f86fae98c226e5e6a50e
Hunter Caron <huntercaron@me.com>
2020-02-16 11:48:01 +0100

    updating repo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-import styled from 'styled-components'
 import 'modern-normalize/modern-normalize.css'
 import './index.css'
 
@@ -14,40 +12,15 @@ import './index.css'
   this is where global styles/font loading lives
 */
 
-
-// styled components
-const Container = styled.div`
-  height: 100%;
-`
-
-const TitleLink = styled(Link) `
-  color: white;
-  text-decoration: none;
-  text-transform: uppercase;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`
-
-const HeaderContainer = styled.div`
-
-`
-
 // page component
 const TemplateWrapper = ({ children }) => (
-  <React.Fragment>
-    <Helmet
-      title="Ted"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+  <>
+    <Helmet>
+      <title>Ted</title>
+    </Helmet>
 
-    {children()}
-
-  </React.Fragment>
+    {children}
+  </>
 )
 
 TemplateWrapper.propTypes = {
@@ -55,7 +28,6 @@ TemplateWrapper.propTypes = {
 }
 
 export default TemplateWrapper
-
 
 /* Global Site Styles
  * used for importing fonts and setting up sizes
